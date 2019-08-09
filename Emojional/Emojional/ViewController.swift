2d725d4f2120d95bdc549a9cf9fd2e71a33b3813
Sofi <sofiajaravata007@gmail.com>
2019-08-09 09:57:09 -0400

    finished

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -20,15 +20,15 @@ class ViewController: UIViewController {
         "🐣" : "Duck Emoji"
     ]
     let custommessages = [
-        "Dog Emoji" : ["You've Got A Friend In Me"],
-        "Lion Emoji" : ["Hakuna Matata"],
-        "Mouse Emoji" : ["Bibbidi Bobbidi Boo"],
-        "Frog Emoji" : ["Kiss The Girl"],
-        "Bunny Emoji" : ["Someday My Prince Will Come"],
-        "Pig Emoji" : ["Paint With All The Colors In The Wind"],
-        "Panda Emoji" : ["Tale As Old As Time"],
-        "Duck Emoji" : ["See The Light"]
-    ]
+        "Dog Emoji" : "You've Got A Friend In Me. This shows that you're an adventurous and loyal spirit with a big heart!",
+        "Lion Emoji" : "Hakuna Matata. This shows that you're a brave and bold person who stops at nohting to protect their friends!",
+        "Mouse Emoji" : "Bibbidi Bobbidi Boo. This shows that you are shy and creative, with a taste for the finer things in life!",
+        "Frog Emoji" : "See The Light. This shows that you like to stand out from the crowd a little and be unique!",
+        "Bunny Emoji" : "Someday My Prince Will Come. This shows that you are sweet and caring person who likes to surround themselves with people they love!",
+        "Pig Emoji" : "Paint With All The Colors In The Wind. This shows that you are a wild chid at heart and love to spend time in nature!",
+        "Panda Emoji" : "Tale As Old As Time. This shows that you are a very intelligent and kind spirit who enjoys family time and reading!",
+        "Duck Emoji" : "Kiss The Girl. This shows that you are a person who enjoys rice krispy treats and football!"
+        ] as [String : Any]
 
     
 
@@ -38,9 +38,9 @@ class ViewController: UIViewController {
     }
     @IBAction func showmessage(sender : UIButton) {
         let selectedemotion = sender.titleLabel?.text
-        let alertController = UIAlertController(title: "You Got:", message: "\(custommessages.values)", preferredStyle: UIAlertController.Style.alert)
+         let emojimessage = custommessages[emojis[selectedemotion!]!]
+        let alertController = UIAlertController(title: "You Got:", message: "\(emojimessage!)", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title :"OK", style : UIAlertAction.Style.default, handler : nil))
-        let emojimessage = custommessages[emojis[selectedemotion!]!]
         present(alertController, animated : true, completion : nil)
     }
 
